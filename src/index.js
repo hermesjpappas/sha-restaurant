687afcb19ff73a18b603f11fba678437a7afb97f
Hermes J. Pappas <hjp@hjp.gr>
2021-11-26 20:05:39 +0200

    Make same thing happen with import

--- src/index.js
+++ src/index.js
@@ -1,23 +1,3 @@
-function makeHeader() {
-    const content = document.getElementById("content");
-    
-    const header = document.createElement("header");
-    
-    const topMenu = document.createElement("div");
-    topMenu.id = "top-menu";
-
-    const storeName = document.createElement("h1");
-    storeName.innerHTML = "&#9876;Murder Grill&#9876";
-    topMenu.appendChild(storeName);
-
-    const banner = document.createElement("div");
-    banner.id = "banner";
-
-    header.appendChild(topMenu);
-    header.appendChild(banner);
-
-    content.appendChild(header);
-}
-
+import { makeHeader } from './header';
 
 makeHeader();
