550aa281aa0e488289b8ca676855e05df40b2e02
Hermes J. Pappas <hjp@hjp.gr>
2021-11-26 23:28:43 +0200

    Make event listener work in main_el

--- src/index.js
+++ src/index.js
@@ -9,34 +9,3 @@ content.appendChild(makeMain());
 // content.appendChild(makeFooter());
 
 
-const labels = document.querySelectorAll('.tab-label');
-const home = document.getElementById("home");
-const menu = document.getElementById("menu");
-const contact = document.getElementById("contact");
-const tab = document.getElementById("tab");
-
-labels.forEach(label => {
-    label.addEventListener('click', (e) => {
-        // console.log(label.id, "was clicked");
-        if(label.id==="home") {
-            label.classList.add("active-h");
-            menu.classList.remove("active-m");
-            contact.classList.remove("active-c");
-            tab.innerHTML = homeContent;
-            
-        }
-        else if(label.id==="menu") {
-            label.classList.add("active-m");
-            home.classList.remove("active-h");
-            contact.classList.remove("active-c");
-            tab.innerHTML = menuContent;
-        }
-
-        else if(label.id==="contact") {
-            label.classList.add("active-c");
-            home.classList.remove("active-h");
-            menu.classList.remove("active-m");
-            tab.innerHTML = contactContent;
-        }
-    });
-});
--- src/main_el.js
+++ src/main_el.js
@@ -1,4 +1,5 @@
-const homeContent = `            <h1>Eat.</h1>
+function makeMain() {
+  const homeContent = `            <h1>Eat.</h1>
             <h2>Or we might just have to murder you...</h2>
             <p>...with our delicious flavors, of course!</p>
             <p>We have the best steak in the whole damn world. We choose the freshest of animals and murder the ever-loving life out of them. We have a long tradition of being the best in the business. We make sure you can <span class="strong">taste</span> the blood. Otherwise...what's the point, am I right?</p>
@@ -7,7 +8,7 @@ const homeContent = `            <h1>Eat.</h1>
             <p>You can find our wonderful restaurant at <a href="https://www.goodreads.com/book/show/8695.The_Restaurant_at_the_End_of_the_Universe" target="_blank">the end of the universe.</a> Depending on the type of meat you're into, the appropriate animal will take your order and point to its cuts so you can <span class="strong">really choose</span> what you want.</p>
             <p>Everything costs <span class="strong">42&euro;</span>. If that doesn't make sense...don't worry about it.</p>`;
 
-const menuContent = `<h1>Choose.</h1>
+  const menuContent = `<h1>Choose.</h1>
             <h2>we won't tell you twice.</h2>
             <p class="price"><span class="strong">Pork:</span> 42&euro;</p>
             <p class="price"><span class="strong">Beef: </span>42&euro;</p>
@@ -16,51 +17,69 @@ const menuContent = `<h1>Choose.</h1>
             <p class="price"><span class="strong">Iguana: </span>42&euro;</p>
             <p class="price"><span class="strong">Human: </span>TBA</p>`;
 
-const contactContent = ` <h1>Talk.</h1>
+  const contactContent = ` <h1>Talk.</h1>
             <h2>or text. or something.</h2>
             <p>You can follow us on social media, send us an email, call us on the phone, write us a letter, send us a carrier pigeon, whatever you want! Just don't complain about anything. You won't get a response from us.</p>
             <p><span class="strong">Phone:</span> 0118999881999119725...3</p>
             <p><span class="strong">Email:</span> murder@murder-grill.murder</p>
             <p><span class="strong">Instagram:</span> @THE-murder-grill</p>
             <p><span class="strong">Address:</span> The End Of The Universe</p>`;
+  const main = document.createElement("main");
 
+  const tabLabels = document.createElement("div");
+  tabLabels.id = "tab-labels";
 
-function makeMain() {
-    const main = document.createElement("main");
+  const home = document.createElement("div");
+  home.id = "home";
+  home.textContent = "home";
+  home.classList.add("tab-label");
+  home.classList.add("active-h");
 
-    const tabLabels = document.createElement("div");
-    tabLabels.id = "tab-labels";
+  const menu = document.createElement("div");
+  menu.id = "menu";
+  menu.textContent = "menu";
+  menu.classList.add("tab-label");
 
-    const home = document.createElement("div");
-    home.id = "home";
-    home.textContent = "home";
-    home.classList.add("tab-label");
-    home.classList.add("active-h");
+  const contact = document.createElement("div");
+  contact.id = "contact";
+  contact.textContent = "contact";
+  contact.classList.add("tab-label");
 
-    const menu = document.createElement("div");
-    menu.id = "menu";
-    menu.textContent = "menu";
-    menu.classList.add("tab-label");
+  tabLabels.appendChild(home);
+  tabLabels.appendChild(menu);
+  tabLabels.appendChild(contact);
 
-    const contact = document.createElement("div");
-    contact.id = "contact";
-    contact.textContent = "contact";
-    contact.classList.add("tab-label");
+  main.appendChild(tabLabels);
 
-    tabLabels.appendChild(home);
-    tabLabels.appendChild(menu);
-    tabLabels.appendChild(contact);
+  const tab = document.createElement("div");
+  tab.id = "tab";
+  tab.innerHTML = homeContent;
 
-    main.appendChild(tabLabels);
+  main.appendChild(tab);
 
-    const tab = document.createElement("div");
-    tab.id = "tab";
-    tab.innerHTML = homeContent;
+  main.addEventListener("click", (e) => {
+    if (e.target.classList.contains("tab-label")) {
+      // console.log(e.target.id, "was clicked");
+      if (e.target.id === "home") {
+        e.target.classList.add("active-h");
+        menu.classList.remove("active-m");
+        contact.classList.remove("active-c");
+        tab.innerHTML = homeContent;
+      } else if (e.target.id === "menu") {
+        e.target.classList.add("active-m");
+        home.classList.remove("active-h");
+        contact.classList.remove("active-c");
+        tab.innerHTML = menuContent;
+      } else if (e.target.id === "contact") {
+        e.target.classList.add("active-c");
+        home.classList.remove("active-h");
+        menu.classList.remove("active-m");
+        tab.innerHTML = contactContent;
+      }
+    }
+  });
 
-    main.appendChild(tab);
-
-
-    return main;
+  return main;
 }
 
-export { makeMain, homeContent, menuContent, contactContent };
+export { makeMain };
