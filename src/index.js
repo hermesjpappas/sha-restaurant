2958d5ae4ef0cbbf6f1a7f53295615122c4afcf5
Hermes J. Pappas <hjp@hjp.gr>
2021-11-26 22:34:39 +0200

    Make tab labels switch visually - success!

--- src/index.js
+++ src/index.js
@@ -8,3 +8,31 @@ const content = document.getElementById("content");
 // content.appendChild(makeMain());
 // content.appendChild(makeFooter());
 
+
+//below code to be transferred to main_el.js
+const labels = document.querySelectorAll('.tab-label');
+const home = document.getElementById("home");
+const menu = document.getElementById("menu");
+const contact = document.getElementById("contact");
+
+labels.forEach(label => {
+    label.addEventListener('click', (e) => {
+        // console.log(label.id, "was clicked");
+        if(label.id==="home") {
+            label.classList.add("active-h");
+            menu.classList.remove("active-m");
+            contact.classList.remove("active-c");
+        }
+        else if(label.id==="menu") {
+            label.classList.add("active-m");
+            home.classList.remove("active-h");
+            contact.classList.remove("active-c");
+        }
+
+        else if(label.id==="contact") {
+            label.classList.add("active-c");
+            home.classList.remove("active-h");
+            menu.classList.remove("active-m");
+        }
+    });
+});
