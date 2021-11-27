acbb5f545b81eef2ebbb28e18ffc409340801ab1
Konstantinos Kompothekras <kobo_50@hotmail.com>
2021-11-27 15:20:13 +0200

    start making footer

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const content = document.getElementById("content");
 
 // content.appendChild(makeHeader());
 // content.appendChild(makeMain());
-// content.appendChild(makeFooter());
+ //content.appendChild(makeFooter());
 
 const home = document.getElementById("home");
 const menu = document.getElementById("menu");
